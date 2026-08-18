8e97047f586551204c79045abcf613824cbbd439
Isabela de Ponte <isabelapnt01@gmail.com>
2026-08-17 22:18:20 -0300

    feat: criação de templates de card de pontos e titulo

--- app/src/main/java/br/edu/ifsp/scl/sc3035018/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3035018/trucoscoreboardcompose/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3035018.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrucoScoreBoardComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +45,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Placar Truco",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(20.dp)
+        )
+
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            ) {
+            Text(
+                text = "Nenhuma Equipe em Mão de 11!!",
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                modifier = Modifier.weight(1f).padding(20.dp)
+            )
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TrucoScoreBoardComposeTheme {
-        Greeting("Android")
+fun MainScreenPreview() {
+    MainScreen()
+}
+
+@Composable
+fun CardTeamPoints(modifier: Modifier = Modifier, teamName: String, teamPointsCounter: Int) {
+    var teamPointsCounter by remember { mutableIntStateOf(0) }
+
+    Column(modifier = Modifier.padding(30.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Team $teamName",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = "10",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(20.dp)
+        )
+        Button(
+            onClick = { teamPointsCounter++ }
+        ) {
+            Text(
+                text = "+1 Ponto",
+                fontSize = 16.sp
+            )
+        }
+        Button(
+            onClick = { teamPointsCounter++ }
+        ) {
+            Text(
+                text = "+3 Pontos",
+                fontSize = 16.sp
+            )
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardTeamPointsPreview() {
+    CardTeamPoints(teamName = "A", teamPointsCounter = 0)
 }
